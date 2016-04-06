c5e69def3a967d0ba0bec0af0752d6fc45f6079b
LuigiVampa <maxschip2306@mail.ru>
2016-04-06 21:27:55 +0300

    DEstroy

---
+++ Source/Presentation/CalculatorPresenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Presentation
+{
+    public class CalculatorPresenter : ICalculatorPresenter
+    {
+        public CalculatorPresenter(ICalculatorView calculatorView)
+        {
+            View = calculatorView;
+            View.Loaded += OnViewLoaded;
+        }
+
+        public ICalculatorView View { get; set; }
+
+        public CalculatorViewModel ViewModel { get; private set; }
+
+        public void Initialize(CalculatorViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            View.DataContext = ViewModel;
+        }
+
+        private void OnViewLoaded(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Source/Presentation/ICalculatorView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Presentation
+{
+    public interface ICalculatorView
+    {
+        object DataContext { get; set; }
+
+        event EventHandler<string> ButtonPressed;
+
+        event EventHandler Loaded;
+    }
+}
